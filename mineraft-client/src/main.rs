224fe09de6b0854499ba97736632ea4993a75c35
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2024-06-09 17:36:17 -0700

    Create basic tcp listener and streamer

--- mineraft-client/src/main.rs
+++ mineraft-client/src/main.rs
@@ -1,3 +1,11 @@
+use std::{
+  io::{Read, Write},
+  net::TcpStream,
+};
+
 fn main() {
-  println!("Hello, world!");
+  let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+
+  stream.write_all(&[1]).unwrap();
+  stream.read_exact(&mut [0; 128]).unwrap();
 }
--- mineraft-tcp/src/main.rs
+++ mineraft-tcp/src/main.rs
@@ -6,17 +6,9 @@ use std::{
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+  let mut buffer = [0; 128];
   for stream in listener.incoming() {
-    let stream = stream.unwrap();
-
-    handle_connection(stream);
+    let mut stream = stream.unwrap();
+    println!("{}", stream.read(&mut buffer).unwrap());
   }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-  let buf_reader = BufReader::new(&mut stream);
-  let http_request: Vec<_> =
-    buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
-
-  println!("Request: {:#?}", http_request);
 }
